dbcd44e5e13fd6f412cc563d7e7f313a98b210d4
wgraham17 <wgraham17@gmail.com>
2015-03-11 19:40:26 -0400

    Updated AssemblyInfo.cs

--- src/SkyModChirpyMaid/SkyModChirpyMaid/Properties/AssemblyInfo.cs
+++ src/SkyModChirpyMaid/SkyModChirpyMaid/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SkyModChirpyMaid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds a Clear Everything button to the Chirper panel.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SkyMod Team (skymod.io)")]
 [assembly: AssemblyProduct("SkyModChirpyMaid")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © SkyModTeam (skymod.io) 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
